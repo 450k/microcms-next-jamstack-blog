6e805a6f65d211d7ff4099dc37de470d4a08d33b
横尾 啓 <yokoo@icd-corp.local>
2026-03-26 15:36:00 +0900

    Vercelのタイムゾーンの調整

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,33 +6,30 @@ import { twMerge } from "tailwind-merge"
 import dayjs from "dayjs"; 
 import timezone from "dayjs/plugin/timezone"; 
 import utc from "dayjs/plugin/utc";
-import ja from 'dayjs/locale/ja'; 
+import "dayjs/locale/ja"; 
 
-
+// 設定
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.locale("ja"); 
+dayjs.tz.setDefault("Asia/Tokyo"); // 全体を日本時間基準に
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-// 日付の設定
-dayjs.extend(utc);
-dayjs.extend(timezone);
-dayjs.locale(ja);　//ロケーションの設定
-
 // 20xx.xx.xx の形式で出力
-export const formatDate = (date: string) => {
-  const formattedDate = dayjs(date).format('YYYY.MM.DD'); // 引数のdateを使う
-  return formattedDate;
+export const formatDate = (date: string | Date) => {
+  // tz() を呼ぶことで、入力された時間が日本時間として解釈・変換
+  return dayjs(date).tz().format('YYYY.MM.DD');
 };
 
 // 00/00の短い形式で出力
-export const formatDateShort = (date: string) => {
-  const formattedDateShort = dayjs(date).format('M/D');
-  return formattedDateShort;
+export const formatDateShort = (date: string | Date) => {
+  return dayjs(date).tz().format('M/D');
 };
 
 // 曜日を出力
-export const formatDay = (date: string) => {
-  const formattedDay = dayjs(date).format('ddd');
-  return formattedDay;
+export const formatDay = (date: string | Date) => {
+  return dayjs(date).tz().format('ddd');
 };
