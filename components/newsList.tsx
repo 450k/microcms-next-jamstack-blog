5f8306c77f49e91c4e33e2d07d3b053f56859661
横尾 啓 <yokoo@icd-corp.local>
2026-03-30 15:47:20 +0900

    fix news table

--- components/newsList.tsx
+++ components/newsList.tsx
@@ -13,14 +13,14 @@ export function NewsItems({ newsItems = [] }: Props) {
 
   return (
     <div className="mb-8">
-      <h3 className="scroll-m-20 border-b pb-2 text-xl font-semibold tracking-tight first:mt-0">
+      <h3 className="scroll-m-20 border-b pb-2 text-xl font-semibold tracking-tight first:mt-0 mb-2">
         お知らせ
       </h3>
       <ul>
         {newsItems.map((news) => (
-          <li key={news.id} className="py-4 ">
-            <div className="grid auto-cols-max grid-flow-col gap-4">
-              <span className="font-medium">📢 {formatDate(news.publishedAt)}</span>
+          <li key={news.id} className="mb-2">
+            <div className="grid grid-cols-1 md:grid-cols-[120px_1fr] gap-0">
+              <span className="font-medium">{formatDate(news.publishedAt)}</span>
               <span className="font-normal line-clamp-2">{news.newsTitle}</span>
               {news.linkTo && (
                 <a href={news.linkTo} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline text-sm self-start">
